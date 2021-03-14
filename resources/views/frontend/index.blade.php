39d2af8cadbfa9fe795e305a6696bc2794b96bf9
Masih Karimi <masihkarimi143@gmail.com>
2021-03-14 14:00:44 +0430

    some changes

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -330,8 +330,8 @@ Afghanistan.</div>
                     <div class="sub-title primary">News Update </div>
                     <h2 class="title mb-0">Latest News & Events</h2>
                 </div>
-                <div class="rs-carousel owl-carousel" data-loop="true" data-items="3" data-margin="30" data-autoplay="true" data-hoverpause="true" data-autoplay-timeout="5000" data-smart-speed="800" data-dots="false" data-nav="false" data-nav-speed="false" data-center-mode="false" data-mobile-device="1" data-mobile-device-nav="false" data-mobile-device-dots="false" data-ipad-device="2" data-ipad-device-nav="false" data-ipad-device-dots="false" data-ipad-device2="1" data-ipad-device-nav2="false" data-ipad-device-dots2="false" data-md-device="3" data-md-device-nav="false" data-md-device-dots="false">
-            @foreach($blogs as $blog)    
+                <div class="rs-carousel owl-carousel" data-loop="false" data-margin="30" data-autoplay="true" data-hoverpause="true" data-autoplay-timeout="5000" data-smart-speed="800" data-dots="false" data-nav="false" data-nav-speed="false" data-center-mode="false" data-mobile-device="1" data-mobile-device-nav="false" data-mobile-device-dots="false" data-ipad-device="2" data-ipad-device-nav="false" data-ipad-device-dots="false" data-ipad-device2="1" data-ipad-device-nav2="false" data-ipad-device-dots2="false" data-md-device="3" data-md-device-nav="false" data-md-device-dots="false">
+            @foreach($blogs as $blog)
                 <div class="blog-item">
                         <div class="image-part">
                             <img src="{{asset('img/blog')}}/{{$blog->image}}" alt="">
@@ -347,8 +347,8 @@ Afghanistan.</div>
                             </ul>
                         </div>
                     </div>
-                </div>
             @endforeach
+           </div>
             </div>
         </div>
         <!-- Blog Section End -->
